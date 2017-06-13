9287b63aef5ef49df489a5ae2cbbb5d377af8a13
Deep Rajput <drb@kanakinfosystems.com>
2017-06-13 10:55:08 +0530

    [IMP] Some improvements for daily expenses.

--- daily_expense/models/daily_expense.py
+++ daily_expense/models/daily_expense.py
@@ -1,12 +1,19 @@
 # -*- coding: utf-8 -*-
-from odoo import api, fields, models, _
+from odoo import api, fields, models, tools, _
 
 
 class Expense(models.Model):
     _name = 'expense.expense'
-    _rec_name = 'month'
 
-    month = fields.Char(required=True)
+    years = [
+        ('2017', '2017'),
+        ('2018', '2018'),
+        ('2019', '2019'),
+        ('2020', '2020'),
+    ]
+
+    year = fields.Selection(years, string='Year', required=True)
+    month = fields.Char(string='Month', required=True)
     expense_lines = fields.One2many('daily.expense', 'expense', string='Daily Expense')
     amount_total = fields.Float(compute='comupte_amount_total', string='Amount Total')
 
@@ -16,13 +23,22 @@ class Expense(models.Model):
             if expense.expense_lines:
                 expense.amount_total = sum(expense.expense_lines.mapped('amount'))
 
+    @api.multi
+    def name_get(self):
+        result = []
+        for record in self:
+            if record.month and record.year:
+                name = "%s - %s" % (record.year, record.month)
+            result.append((record.id, name))
+        return result
+
 
 class DailyExpense(models.Model):
     _name = 'daily.expense'
 
     name = fields.Char(required=True)
     expense_category = fields.Many2one('expense.category', string="Expense Category", required=True)
-    date = fields.Date(required=True)
+    date = fields.Date(required=True, default=fields.Date.context_today)
     amount = fields.Float(required=True)
     mode = fields.Selection([('offline', 'Offline'), ('online', 'Online')], string="Mode", required=True, default='offline')
     bank = fields.Many2one('bank.account')
@@ -33,3 +49,10 @@ class ExpenseCategory(models.Model):
     _name = 'expense.category'
 
     name = fields.Char(required=True)
+    amount_total = fields.Float(compute='compute_amount_total', string='Amount Total')
+
+    @api.multi
+    def compute_amount_total(self):
+        for rec in self:
+            expenses = self.env['daily.expense'].search([('expense_category', '=', rec.id)])
+            rec.amount_total = sum(expenses.mapped('amount'))
